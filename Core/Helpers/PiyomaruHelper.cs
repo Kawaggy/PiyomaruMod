6b2ebc7d9191b6612e637601c4d3e4de5a957b99
Kawaggy <emorei.game@gmail.com>
2021-07-29 01:30:17 +0100

    Make extension for player

---
+++ Core/Helpers/PiyomaruHelper.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+using PiyomaruMod.Common;
+
+namespace PiyomaruMod.Core.Helpers
+{
+    public static partial class PiyomaruHelper
+    {
+        public static PiyomaruPlayer Piyomaru(this Player player)
+        {
+            return player.GetModPlayer<PiyomaruPlayer>();
+        }
+    }
+}
